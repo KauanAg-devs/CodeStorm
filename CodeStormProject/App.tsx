0587f5f63bff9c5babb96761cc1978ef26d0f659
JotaPe8 <joaopedrolopessardinha@gmail.com>
2023-09-06 16:08:36 -0300

    Funcionando!

--- CodeStormProject/App.tsx
+++ CodeStormProject/App.tsx
@@ -1,13 +1,9 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
-import { NavigationContainer } from "@react-navigation/native"; 
-import Navegar from './src/Routes/Navegar'
+import React from "react"
+import { SafeAreaView, Text } from "react-native"
 export default function App(){
   return(
-    <SafeAreaView style={{flex: 1}}>
-      <NavigationContainer>
-          <Navegar />
-      </NavigationContainer>
+    <SafeAreaView>
+      <Text>Hello World</Text>
     </SafeAreaView>
   )
 }
